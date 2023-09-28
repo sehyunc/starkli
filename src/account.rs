d0503419c0517c958b7c3de6a7b53fa211a3867b
Sehyun Chung <41171808+sehyunc@users.noreply.github.com>
2023-09-28 11:48:14 -0700

    add katana account class hash

--- src/account.rs
+++ src/account.rs
@@ -12,21 +12,26 @@ use starknet::{
 
 const BRAAVOS_SIGNER_TYPE_STARK: FieldElement = FieldElement::ONE;
 
-pub const KNOWN_ACCOUNT_CLASSES: [KnownAccountClass; 3] = [
+pub const KNOWN_ACCOUNT_CLASSES: [KnownAccountClass; 4] = [
     KnownAccountClass {
         class_hash: felt!("0x048dd59fabc729a5db3afdf649ecaf388e931647ab2f53ca3c6183fa480aa292"),
         variant: AccountVariantType::OpenZeppelin,
         description: "OpenZeppelin account contract v0.6.1 compiled with cairo-lang v0.11.0.2",
     },
     KnownAccountClass {
-        class_hash: felt!("0x025ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918"),
-        variant: AccountVariantType::Argent,
-        description: "Argent X official proxy account",
+        class_hash: felt!("0x04d07e40e93398ed3c76981e72dd1fd22557a78ce36c0515f679e27f0bb5bc5f"),
+        variant: AccountVariantType::OpenZeppelin,
+        description: "OpenZeppelin account contract v0.5.0 compiled with cairo-lang v0.10.1",
     },
     KnownAccountClass {
         class_hash: felt!("0x03131fa018d520a037686ce3efddeab8f28895662f019ca3ca18a626650f7d1e"),
         variant: AccountVariantType::Braavos,
         description: "Braavos official proxy account",
+    },
+    KnownAccountClass {
+        class_hash: felt!("0x01a736d6ed154502257f02b1ccdf4d9d1089f80811cd6acad48e6b6a9d1f2003"),
+        variant: AccountVariantType::Argent,
+        description: "Argent X official account",
     },
 ];
 
